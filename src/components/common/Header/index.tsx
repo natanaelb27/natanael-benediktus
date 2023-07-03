d2f6ac9ba98119f19f5f3022ab00becfa9f40842
Natanael Benediktus <natanael.benediktus@shopee.com>
2023-07-03 10:21:36 +0700

    add project detail when only one picture

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -8,9 +8,11 @@ import {
   handleClickScroll,
   handleScrollToTop,
 } from "../../../utils/clickScroll";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const [isMenuClicked, setIsMenuClicked] = useState<boolean>(false);
 
@@ -18,54 +20,76 @@ const Header = () => {
     setIsMenuClicked(!isMenuClicked);
   };
 
-  return (
-    <header className="py-5 px-5 sm:px-24 md:px-52 bg-base-100 border-b-2 shadow-md sticky top-0 z-10">
-      <div className="flex items-center justify-between">
-        <a href="/" className="text-3xl font-semibold">
-          Natan<span className="text-red-400">.</span>
-        </a>
-        <div className="hidden md:flex justify-start gap-5">
-          <span
-            onClick={() => handleScrollToTop()}
-            className="font-semibold hover:text-red-400 cursor-pointer"
-          >
-            Home
-          </span>
-          <span
-            onClick={() => handleClickScroll("about-section")}
-            className="font-semibold hover:text-red-400 cursor-pointer"
-          >
-            About
-          </span>
-          <span
-            onClick={() => handleClickScroll("experience-section")}
-            className="font-semibold hover:text-red-400 cursor-pointer"
-          >
-            Experience
-          </span>
-          <span
-            onClick={() => handleClickScroll("project-section")}
-            className="font-semibold hover:text-red-400 cursor-pointer"
-          >
-            Project
-          </span>
+  const isHome = window.location.pathname === "/";
+
+  if (isHome) {
+    return (
+      <header className="py-5 px-5 sm:px-24 md:px-52 bg-base-100 border-b-2 shadow-md sticky top-0 z-10">
+        <div className="flex items-center justify-between">
+          <a href="/" className="text-3xl font-semibold">
+            Natan<span className="text-red-400">.</span>
+          </a>
+          <div className="hidden md:flex justify-start gap-5">
+            <span
+              onClick={() => handleScrollToTop()}
+              className="font-semibold hover:text-red-400 cursor-pointer"
+            >
+              Home
+            </span>
+            <span
+              onClick={() => handleClickScroll("about-section")}
+              className="font-semibold hover:text-red-400 cursor-pointer"
+            >
+              About
+            </span>
+            <span
+              onClick={() => handleClickScroll("experience-section")}
+              className="font-semibold hover:text-red-400 cursor-pointer"
+            >
+              Experience
+            </span>
+            <span
+              onClick={() => handleClickScroll("project-section")}
+              className="font-semibold hover:text-red-400 cursor-pointer"
+            >
+              Project
+            </span>
+          </div>
+          <div className="flex md:hidden">
+            <Button
+              shape="circle"
+              color="ghost"
+              onClick={() => {
+                dispatch(mobileMenuClicked());
+                menuClicked();
+              }}
+            >
+              <CgMenu className="text-2xl" />
+            </Button>
+          </div>
         </div>
-        <div className="flex md:hidden">
-          <Button
-            shape="circle"
-            color="ghost"
-            onClick={() => {
-              dispatch(mobileMenuClicked());
-              menuClicked();
-            }}
-          >
-            <CgMenu className="text-2xl" />
-          </Button>
+        <MobileMenu showModal={isMenuClicked} onClose={menuClicked} />
+      </header>
+    );
+  } else {
+    return (
+      <header className="py-5 px-5 sm:px-24 md:px-52 bg-base-100 border-b-2 shadow-md sticky top-0 z-10">
+        <div className="flex items-center justify-between">
+          <a href="/" className="text-3xl font-semibold">
+            Natan<span className="text-red-400">.</span>
+          </a>
+          <div className="hidden md:flex justify-start gap-5">
+            <span
+              onClick={() => navigate("/")}
+              className="font-semibold hover:text-red-400 cursor-pointer"
+            >
+              Home
+            </span>
+          </div>
         </div>
-      </div>
-      <MobileMenu showModal={isMenuClicked} onClose={menuClicked} />
-    </header>
-  );
+      </header>
+    );
+  }
 };
 
 export default Header;
--- src/pages/ProjectDetail/index.tsx
+++ src/pages/ProjectDetail/index.tsx
@@ -1,15 +1,124 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Button, Card, Carousel } from "react-daisyui";
 import { FaArrowRight } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
-import ProjectData from "../../data/project.json";
+import ProjectDetailData from "../../data/projectDetail.json";
 
 const ProjectDetail = () => {
   const navigate = useNavigate();
   const { projectName } = useParams();
+  const projectDetailData = ProjectDetailData.find((data) => {
+    return data.project_name === projectName;
+  });
+
+  useEffect(() => {
+    if (projectDetailData === undefined) {
+      navigate("/");
+    }
+  }, [navigate, projectDetailData]);
+
+  const [isPictureHovered, setIsPictureHovered] = useState(false);
+
+  const onMouseEnterPicture = () => {
+    setIsPictureHovered(true);
+  };
+
+  const onMouseLeavePicture = () => {
+    setIsPictureHovered(false);
+  };
+
+  const buttonStyle = (value: string) => {
+    let translateX = "";
+    if (value === "❮") {
+      translateX = "-translate-x-2";
+    }
+
+    if (value === "❯") {
+      translateX = "translate-x-2";
+    }
+    return (
+      <Button
+        shape="circle"
+        className={`hidden md:block border-0 bg-transparent translate-x-0 ${
+          isPictureHovered
+            ? `${translateX} bg-slate-100 hover:bg-slate-100`
+            : ""
+        }`}
+      >
+        {value}
+      </Button>
+    );
+  };
+
+  const Gallery = () => {
+    if (projectDetailData?.gallery === undefined) {
+      return <></>;
+    }
+
+    if (projectDetailData?.gallery?.length > 1) {
+      return (
+        <div>
+          <p className="mt-2 mb-0 font-bold lg:text-2xl text-xl">Gallery</p>
+          <Carousel
+            className="md:rounded-box mt-4"
+            display="sequential"
+            buttonStyle={buttonStyle}
+          >
+            {projectDetailData?.gallery?.map((data) => {
+              const picture = require("../../assets/images/" + data.picture);
+              return (
+                <Carousel.Item
+                  onMouseEnter={onMouseEnterPicture}
+                  onMouseLeave={onMouseLeavePicture}
+                  className="w-full border-2"
+                  src={picture}
+                  alt={data.description}
+                ></Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+      );
+    }
+
+    const projectDetailGallery = projectDetailData?.gallery?.at(0);
+    const picture = require("../../assets/images/" +
+      projectDetailGallery?.picture);
+
+    return (
+      <div>
+        <p className="mt-2 mb-0 font-bold lg:text-2xl text-xl">Gallery</p>
+        <img
+          src={picture}
+          alt={projectDetailGallery?.description}
+          className="rounded-xl mt-4"
+        ></img>
+      </div>
+    );
+  };
 
   return (
-    <div>
-      <div>{projectName}</div>
+    <div className="flex flex-col gap-5 mx-auto px-8 mt-8 lg:w-1/2">
+      <h1 className="font-semibold text-3xl">
+        {projectDetailData?.project_name}
+      </h1>
+      <p className="my-0 font-light lg:text-base text-sm italic shadow-[inset_3px_0_0_0_#444452] -ml-4 pl-4">
+        {projectDetailData?.brief}
+      </p>
+      <p className="mt-2 mb-0 font-bold lg:text-2xl text-xl">Project Summary</p>
+      <p className="my-0 font-light lg:text-base text-sm">
+        {projectDetailData?.project_description}
+      </p>
+      <div>{Gallery()}</div>
+      <div className="flex gap-2 my-4 flex-wrap">
+        {projectDetailData?.tools.map((tool, i) => {
+          return (
+            <p className="my-0 lg:text-base text-sm py-1 px-2 border-dark border rounded font-medium font-inconsolata">
+              {tool}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 };
